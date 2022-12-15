816d398822fa9ceea8ba95ba3e4a1bcb86a5e520
Vijay Kumar <vijay1021.gummadi@gmail.com>
2022-12-15 00:23:47 -0500

    f
    v

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
 
-const routes: Routes = [];
+imports: [
+  FormsModule,
+  BrowserModule,
+  AppRoutingModule,
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+],
+
+	providers
+}: [],
+bootstrap: [AppComponent]
 })
-export class AppRoutingModule { }
+export class AppModule { }
+export { AppRoutingModule };
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+],
+
+
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
-  providers: [],
+  providers: [
+
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
