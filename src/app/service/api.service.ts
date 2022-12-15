c15bd1514f9d7867376953c0c11e113e3aca5967
Vijay Kumar <vijay1021.gummadi@gmail.com>
2022-12-15 08:40:14 -0500

    hhh

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -22,5 +22,20 @@ export class ApiService {
         return res;
       })
     );
+
+    // updateEmploye(data : any,id: number){
+    //   return this.http.put<any>('http://localhost:3000/posts'+id,data).pipe(
+    //     map(res:any)=>{
+    //     return res;
+    //   })
+    // );
+    // deleteEmploye(id: number){
+    //   return this.http.delete<any>('http://localhost:3000/posts' +id)
+    //   .pipe(map((res:any)=>{
+    //     return res;
+    //   }))
   }
+
 }
+
+
